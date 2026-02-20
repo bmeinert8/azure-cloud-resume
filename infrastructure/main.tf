c751f491330818b6e476334b2089702b889af773
Bmeinert8 <brian.meinert1@gmail.com>
2026-02-19 20:07:19 -0500

    update to terraform to add python function app, app service plan, and storage account for function app

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -81,3 +81,51 @@ resource "azurerm_cosmosdb_sql_container" "container" {
   database_name         = azurerm_cosmosdb_sql_database.sql_db.name
   partition_key_path    = "/id"
 }
+
+#8. Storage Account for the Function App
+resource "azurerm_storage_account" "func_sa" {
+  name                     = "brianfuncsa440395"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+#9. App Service Plan (Serverless Consumption Plan)
+resource "azurerm_service_plan" "app_plan" {
+  name                = "brian-resume-app-plan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "Y1" # Y1 is the dynamic/serverless tier
+}
+
+# 10. The Python Function App
+resource "azurerm_linux_function_app" "func_app" {
+  name                       = "brian-resume-api-440395"
+  resource_group_name        = azurerm_resource_group.rg.name
+  location                   = azurerm_resource_group.rg.location
+  service_plan_id            = azurerm_service_plan.app_plan.id
+  storage_account_name       = azurerm_storage_account.func_sa.name
+  storage_account_access_key = azurerm_storage_account.func_sa.primary_access_key
+
+  site_config {
+    application_stack {
+      python_version = "3.12" # Update this to local Python version if needed
+    }
+    cors {
+      allowed_origins = ["*"] # will lock this down to just website URL later
+    }
+  }
+
+  app_settings = {
+    # Terraform magically injects the connection string here! No copy-pasting required.
+    "CosmosDbConnectionString" = azurerm_cosmosdb_account.db.primary_sql_connection_string
+    "AzureWebJobsFeatureFlags" = "EnableWorkerIndexing" # Required for the Python V2 model
+  }
+}
+
+# 11. Output the Live API URL
+output "api_url" {
+  value = "https://${azurerm_linux_function_app.func_app.default_hostname}/api/GetResumeCounter"
+}
